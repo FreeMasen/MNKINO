3b92127cc2995c6c20b65b9abda2fbfdfe8c6f93
freemasen <r.f.masen@gmail.com>
2016-01-01 21:10:39 -0600

    Static file expanded, still not showing.

--- src/MNKINO/ViewModels/ContactViewModel.cs
+++ src/MNKINO/ViewModels/ContactViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,16 @@ namespace MNKINO.ViewModels
 {
     public class ContactViewModel
     {
+        [Required]
+        [StringLength(255,MinimumLength = 3)]
         public string From { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(1024,MinimumLength = 10)]
         public string Message { get; set; }
     }
 }
